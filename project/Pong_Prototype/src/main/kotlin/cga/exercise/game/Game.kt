45b343b6dfa244f1cd08e3492c7d3da40fe6a003
mmuell10 <62564767+mmuell10@users.noreply.github.com>
2021-08-22 14:52:04 +0200

    Text + Name Changes

--- project/Pong_Prototype/src/main/kotlin/cga/exercise/game/Game.kt
+++ project/Pong_Prototype/src/main/kotlin/cga/exercise/game/Game.kt
@@ -6,7 +6,7 @@ class Game(width: Int,
            height: Int,
            fullscreen: Boolean = false,
            vsync: Boolean = false,
-           title: String = "PONG",
+           title: String = "AT0M1C-JUNGL3-W0RM3-BATLL3",
            GLVersionMajor: Int = 3,
            GLVersionMinor: Int = 3) : GameWindow(width, height, fullscreen, vsync, GLVersionMajor, GLVersionMinor, title, 4, 120.0f) {
 
